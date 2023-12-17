6f1e5621802a6bdf1fc02335403a45fd0deb4647
BAHAA <bahaaadel47880@gmail.com>
2023-12-17 06:26:35 +0200

    alias

---
+++ 0x01-python-if_else_loops_functions/6-print_comb6.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+for i in range(9):
+    for j in range(10):
+        if j == i:
+            continue
+        result = f"{i}{j}"
+        print(result, end='\n' if result == '89' else ', ')
