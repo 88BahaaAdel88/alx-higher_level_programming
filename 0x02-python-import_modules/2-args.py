4e22f4a14cf7c4266f5be918b386fd42ed194ae4
BAHAA <bahaaadel47880@gmail.com>
2024-01-11 19:11:30 +0200

    alias

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,9 @@ from sys import argv
 argc = len(argv)
 i = 1
 if argc == 1:
-    print('0 arguments.'.format(argc))
+    print('0 arguments.')
+elif argc == 2:
+    print('{} argument: {}'.format((argc - 1), argv[i]))
 else:
     print('{} arguments:'.format(argc - 1))
     while i < argc:
