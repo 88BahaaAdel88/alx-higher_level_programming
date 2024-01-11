3e4ac006e56cd40eb1da380300c97bb23163aba0
BAHAA <bahaaadel47880@gmail.com>
2024-01-11 19:13:37 +0200

    alias

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-from sys import argv
+if __name__ == "__main__":
+    from sys import argv
 
-argc = len(argv)
-i = 1
-if argc == 1:
-    print('0 arguments.')
-elif argc == 2:
-    print('{} argument: {}'.format((argc - 1), argv[i]))
-else:
-    print('{} arguments:'.format(argc - 1))
-    while i < argc:
-        print('{}: {}'.format(i, argv[i]))
-        i = i + 1
+    argc = len(argv)
+    i = 1
+    
+    if argc == 1:
+        print('0 arguments.')
+    elif argc == 2:
+        print('{} argument: {}'.format((argc - 1), argv[i]))
+    else:
+        print('{} arguments:'.format(argc - 1))
+        while i < argc:
+            print('{}: {}'.format(i, argv[i]))
+            i = i + 1
